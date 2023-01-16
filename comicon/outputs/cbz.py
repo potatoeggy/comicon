87e0c02e3553a87beaa84a55dac446207fd98a5d
eggy <danielchen04@hotmail.ca>
2023-01-15 22:52:29 -0500

    feat: add cir-cbz adapter
    from mandown

--- comicon/outputs/cbz.py
+++ comicon/outputs/cbz.py
@@ -1,4 +1,10 @@
+import zipfile
 from pathlib import Path
+
+from lxml import etree
+from lxml.builder import E
+
+from .. import cirtools
 
 
 def create_comic(cir_path: Path, dest: Path) -> None:
@@ -8,3 +14,39 @@ def create_comic(cir_path: Path, dest: Path) -> None:
     :param `cir_path`: The path to the IR folder.
     :param `dest`: The path to the destination file.
     """
+    comic = cirtools.read_metadata(cir_path)
+
+    tree = E.ComicInfo(
+        E.Title(
+            comic.metadata.title,
+        ),
+        E.Summary(comic.metadata.description),
+        E.Writer(", ".join(comic.metadata.authors)),
+        E.Genre(", ".join(comic.metadata.genres)),
+        # TODO: add pages
+    )
+    text_xml = etree.tostring(
+        tree, pretty_print=True, encoding="utf-8", xml_declaration=True
+    ).decode()
+
+    with zipfile.ZipFile(dest, "w", zipfile.ZIP_DEFLATED) as file:
+        for i, chap in enumerate(comic.chapters):
+            chap_path = cir_path / chap.slug
+            rel_chap_path = chap_path.with_name(f"{i:05}-{chap.slug}")
+            for image in sorted(chap_path.iterdir()):
+                new_img_path = rel_chap_path / image.name
+                file.write(
+                    image,
+                    new_img_path.absolute().relative_to(cir_path),
+                    zipfile.ZIP_DEFLATED,
+                )
+
+        if comic.metadata.cover_path_rel:
+            cover_path = cir_path / comic.metadata.cover_path_rel
+            file.write(
+                cover_path,
+                # rename cover to appear first in the archive
+                cover_path.with_stem("__cover").absolute().relative_to(cir_path),
+                zipfile.ZIP_DEFLATED,
+            )
+        file.writestr("ComicInfo.xml", text_xml)
--- tests/test_cirtools.py
+++ tests/test_cirtools.py
@@ -3,5 +3,5 @@ import comicon
 
 def test_cirtools():
     comicon.create_comic(
-        "/home/eggy/Kaguya-sama - Love Is War", "/home/eggy/kaguya.pdf", "pdf"
+        "/home/eggy/Kaguya-sama - Love Is War", "/home/eggy/kaguya.cbz", "cbz"
     )
